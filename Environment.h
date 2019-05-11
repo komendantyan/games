85dd913db3667b726f7d11d0f53e2aa0dc875ec6
Andrey Komendantyan <komendantyan@gmail.com>
2019-05-12 02:59:33 +0300

    move environment to separate file

---
+++ Environment.h
@@ -0,0 +1,211 @@
+#include <deque>
+#include <algorithm>
+#include <random>
+
+
+enum class Tile {
+    EMPTY,  // EMPTY,
+    APPLE,  // RED,
+    SNAKE,  // BLUE,
+    HEAD,  // GREEN,
+    FIELD,  // YELLOW,
+    NIGHT  // BLACK,
+};
+
+
+// TODO think about queue of action concept ?
+class Environment {
+public:
+    using Battlefield = std::vector<std::vector<Tile>>;
+    using State = Battlefield;
+
+    enum class Action {
+        TURN_RIGHT,
+        TURN_UP,
+        TURN_LEFT,
+        TURN_DOWN,
+        SWAP_HEAD
+    };
+
+    enum class Direction {
+        RIGHT,
+        UP,
+        LEFT,
+        DOWN,
+    };
+
+public:
+    Environment(size_t width, size_t height) :
+        _width(width),
+        _height(height),
+        battlefield(height, std::vector<Tile>(width, Tile::FIELD)),
+        game_over(false),
+        block_action(false)
+    {
+        _snake = {{2, 0}, {1, 0}, {0, 0}};
+        last_tail_pos = {0, 1};
+
+        for (auto pos : _snake) {
+            battlefield[pos.y][pos.x] = Tile::SNAKE;
+        }
+        battlefield[_snake.front().y][_snake.front().x] = Tile::HEAD;
+
+        std::mt19937 mersenne(std::random_device{}());
+        std::uniform_int_distribution<size_t> distr_width(0, width - 1),
+                                              distr_height(1, height - 1);
+
+        for (int i = 0; i < 15; ++i) {
+            size_t y = distr_height(mersenne);
+            size_t x = distr_width(mersenne);
+            battlefield[y][x] = Tile::APPLE;
+        }
+
+        current_direction = Direction::RIGHT;
+    }
+
+    void act(Action action) {
+        if (block_action) {
+            return;
+        }
+
+        if (action == Action::SWAP_HEAD) {
+            std::reverse(_snake.begin(), _snake.end());
+            current_direction = getDirectionFromTwoVector(_snake.front(),
+                                                          last_tail_pos);
+        } else {
+            const Direction forbidden_direction = aboutFace(current_direction);
+            Direction proposal_direction;
+            switch (action) {
+                case Action::TURN_RIGHT: {
+                     proposal_direction = Direction::RIGHT;
+                     break;
+                }
+                case Action::TURN_UP: {
+                     proposal_direction = Direction::UP;
+                     break;
+                }
+                case Action::TURN_LEFT: {
+                     proposal_direction = Direction::LEFT;
+                     break;
+                }
+                case Action::TURN_DOWN: {
+                     proposal_direction = Direction::DOWN;
+                     break;
+                }
+            }
+
+            if (forbidden_direction != proposal_direction) {
+                current_direction = proposal_direction;
+            }
+        }
+
+        block_action = true;
+    }
+
+    void step() {
+        if (game_over) {
+            return;
+        }
+
+        const sf::Vector2u& head = _snake.front();
+        sf::Vector2u new_head;
+
+        switch (current_direction) {
+            case Direction::RIGHT: {
+                if (head.x == _width - 1) {
+                    game_over = true;
+                    return;
+                }
+                new_head = _snake.front() + sf::Vector2u{1, 0};
+                break;
+            }
+            case Direction::UP: {
+                if (head.y == 0) {
+                    game_over = true;
+                    return;
+                }
+                new_head = _snake.front() - sf::Vector2u{0, 1};
+                break;
+            }
+            case Direction::LEFT: {
+                if (head.x == 0) {
+                    game_over = true;
+                    return;
+                }
+                new_head = _snake.front() - sf::Vector2u{1, 0};
+                break;
+            }
+            case Direction::DOWN: {
+                if (head.y == _height - 1) {
+                    game_over = true;
+                    return;
+                }
+                new_head = _snake.front() + sf::Vector2u{0, 1};
+                break;
+            }
+        }
+
+        switch (battlefield[new_head.y][new_head.x]) {
+            case Tile::SNAKE: {
+                game_over = true;
+                break;
+            }
+            case Tile::APPLE: {
+                battlefield[_snake.front().y][_snake.front().x] = Tile::SNAKE;
+                _snake.push_front(new_head);
+                battlefield[new_head.y][new_head.x] = Tile::HEAD;
+                break;
+            }
+            case Tile::FIELD: {
+                battlefield[_snake.back().y][_snake.back().x] = Tile::FIELD;
+                last_tail_pos = _snake.back();
+                _snake.pop_back();
+                battlefield[_snake.front().y][_snake.front().x] = Tile::SNAKE;
+                _snake.push_front(new_head);
+                battlefield[new_head.y][new_head.x] = Tile::HEAD;
+                break;
+            }
+        }
+        block_action = false;
+    }
+
+    State getState() {
+        return battlefield;
+    }
+
+private:
+    std::deque<sf::Vector2u> _snake;
+    Battlefield battlefield;
+    size_t _width;
+    size_t _height;
+    Direction current_direction;
+    bool game_over;
+    bool block_action;
+    sf::Vector2u last_tail_pos;
+
+private:
+    static Direction aboutFace(Direction dir) {
+        return static_cast<Direction>(
+            (static_cast<size_t>(dir) + 2) % 4);
+    }
+
+    static Direction getDirectionFromTwoVector(const sf::Vector2u& lhs,
+                                               const sf::Vector2u& rhs) {
+        if (lhs.x == rhs.x) {
+            if (lhs.y == rhs.y) {
+                throw std::invalid_argument("Equal lhs and rhs");
+            } else if (lhs.y < rhs.y) {
+                return Direction::DOWN;
+            } else {
+                return Direction::UP;
+            }
+        } else if (lhs.y != rhs.y) {
+            throw std::invalid_argument("lhs and rhs not on same horizontal or vertical");
+        } else if (lhs.x < rhs.x) {
+            return Direction::RIGHT;
+        } else {
+            return Direction::LEFT;
+        }
+    }
+};
+
